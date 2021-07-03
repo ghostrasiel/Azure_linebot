555c6a5345305cdffc58641bf87d7711ff07aedf
ghostrasiel <ghostrasiel@gmail.com>
2021-07-03 19:04:23 +0800

    great ok

--- Linebot.py
+++ Linebot.py
@@ -5,6 +5,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage , ImageSen
 from linebot.exceptions import InvalidSignatureError
 import json
 import os 
+import datetime
+import re
 
 file = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__)
@@ -43,7 +45,7 @@ def callback():
 def handle_message(event):
     message_type = event.message.type
     user_id = event.source.user_id
-    print('123')
+    profile = line_bot_api.get_profile(user_id) #取得個人資料
     if message_type =='image':
         message_id = event.message.id
         message_content = line_bot_api.get_message_content(message_id)
@@ -56,29 +58,37 @@ def handle_message(event):
 
         image_path = f'{file}/static/{fileName}'
         # image_path = f'https://{request.host}/static/{fileName}' #取得圖片本機網址
-        print(image_path)
 
         number_id = formrecognizer_by_local.formrecognizer_by_local(image_path)
-        # number_id = formrecognizer_by_url.formrecognizer_by_url(image_path)
         invoice_date = number_id['日期']     
-        print(invoice_date)  
-        invoice_number = number_id['發票號碼']  
-        data = selectdb.Mongo_select(int(invoice_date))
-        # print(data)
-        ans = receipt.receipt_mechine([invoice_number] , data)
-        # print(ans[0][0])
-
-
-        if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='你的發票'+ans[0][0]+ans[0][1]+ans[0][2]))
-            # line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=image_path ,preview_image_url=image_path))
+        print(invoice_date)
+        invoice_number = number_id['發票號碼']
+        print(invoice_number)
+        D = datetime.datetime.today().strftime('%m%d')
+        today = int(D)
+        date_x = int(str(int(invoice_date)+2)[3:]+str('25'))
+        if today > date_x  :
+            data = selectdb.Mongo_select(int(invoice_date))
+            # print(data)
+            ans = receipt.receipt_mechine([invoice_number] , data)
+            # print(ans[0][0])
+            if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f'{profile.display_name} , 你的發票'+ans[0][0]+ans[0][1]+ans[0][2]))
+                # line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=image_path ,preview_image_url=image_path))
+                print('ok')
+        else:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f'{profile.display_name},你的發票還未開獎請{str(date_x)}後再來'))
             print('ok')
 
         # print(event.message.text) # 接收用戶訊息
 
     elif message_type== 'text':
-        message = event.message.text
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '請給我發票'))
+        status = profile.status_message #狀態消息
+        repl = []
+        repl.append(TextSendMessage(text = f'{profile.display_name}請給我發票,好嗎'))
+        if (status != None) and (re.search('\(.+\)' , status) == None):
+            repl.append(TextSendMessage(text = f'我在監視你:{status}'))
+        line_bot_api.reply_message(event.reply_token, repl)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "error"))
 
--- model/formrecognizer_by_local.py
+++ model/formrecognizer_by_local.py
@@ -1,28 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
-
-
-# by local
+import os
+from azure.ai.formrecognizer import FormRecognizerClient
+from azure.core.credentials import AzureKeyCredential
 
 def formrecognizer_by_local(local_image_path):
-    import os
-    from azure.core.exceptions import ResourceNotFoundError
-    from azure.ai.formrecognizer import FormRecognizerClient
-    from azure.ai.formrecognizer import FormTrainingClient
-    from azure.core.credentials import AzureKeyCredential
-
     # Set user key, endpoint
-    key = "653310d794cd4bcba674b91918a7dc72"
-    endpoint = "https://tfb102-t4-project.cognitiveservices.azure.com/"
+    key = "55334d079b9949739f108b11dbee454b"
+    endpoint = r"https://formtfb1024.cognitiveservices.azure.com/"
 
     # call API
     form_recognizer_client = FormRecognizerClient(endpoint, AzureKeyCredential(key))
-    trained_model_id = "240e58da-7514-4e22-9ac4-efd6705d8280"
+    trained_model_id = "d16c7bd1-d2db-4b2c-9a1f-e8064d821638"
 
     # image location
-#     local_image_path = os.getcwd() + '/photo/test/test__42385416.jpg'
+    # local_image_path = os.getcwd() + '/photo/test/test__42385416.jpg'
 
     # 讀取圖片 (二進位)
     local_image = open(local_image_path, "rb")
@@ -35,19 +26,24 @@ def formrecognizer_by_local(local_image_path):
 
     for recognized_form in result:
         print("Form type: {}".format(recognized_form.form_type))
-    #     print(recognized_form.fields)
+        print(recognized_form.fields)
+        print('---------------------')
         for name, field in recognized_form.fields.items():
             if name not in output:
                 output[name]= str(field.value)
             else:
                 output[name].append(field.value)
-
-    output["年份"] = output["年份"][0:3]
-    output["月份"] = output["月份"].split('-')[0].zfill(2)
-    output["發票號碼"] = output["發票號碼"][-8:]
+    
+    output["年份"] = output["年"][0:3]
+    output["月份"] = output["月"].split('-')[0].zfill(2)
+    output["發票號碼"] = output["號碼"][-8:]
     output["日期"] = output["年份"][0:3]+output["月份"].split('-')[0].zfill(2)
     return output
     
 # if __name__ == '__main()__':
 #     formrecognizer_by_local(local_image_path)
+
+
+# local_image_path = "C:\\Users\\Tibame_25\\Desktop\\Line_bot\\發票模組訓練\\測試\\S__19996689.jpg"
+# print(formrecognizer_by_local(local_image_path))
 
